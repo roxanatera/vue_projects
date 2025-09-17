548ec7b9614ea4180c1d89b70c2a95600702b864
Julia Roxana Natera <jrnt17@gmail.com>
2025-09-17 17:05:27 +0200

    Component 'contador' created

---
+++ src/components/Contador.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>Hello Everyone</h1>
+ <p> {{ contador }}</p>
+<button @click="incrementar">Incrementar</button> <span></span>
+<button @click="decrementar">Decrementar</button>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const contador = ref(0);
+const incrementar = () => {
+  contador.value++;
+}
+
+const decrementar = () => {
+  contador.value--;
+}
+
+</script>
+
+<style scoped>
+p {
+  color:blue;
+}
+</style>
